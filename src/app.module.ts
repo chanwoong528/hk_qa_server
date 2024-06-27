05baa2060634b6d04f59507c5a70daa4d765a341
문찬웅 <mooncw@hankookilbo.com>
2024-06-27 16:46:34 +0900

    setup progress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as path from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import configuration from "../config/configuration"
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `env/${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        retryAttempts: configService.get('NODE_ENV') === 'prod' ? 10 : 1,
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: Number(configService.get('POSTGRES_PORT_POOL')),
+        database: configService.get('POSTGRES_DB'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        entities: [
+          path.join(__dirname, 'src/entities/**/*.entity.{js, ts}'),
+        ],
+        synchronize: false,
+        logging: true,
+        timezone: 'local',
+      }),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+
+    console.log("!!!")
+
+    
     return 'Hello World!';
   }
 }
