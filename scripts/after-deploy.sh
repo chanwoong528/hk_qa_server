85c688c47ad08022d934c8573438abc7c5168bb8
문찬웅 <mooncw@hankookilbo.com>
2024-08-05 17:12:28 +0900

    npm not found codedeploy fixed

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -2,7 +2,9 @@
 REPOSITORY=/home/ubuntu/build
 
 cd $REPOSITORY
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 npm install
-
 pm2 start dist
