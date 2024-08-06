74d1465303f2dac870fddac8f25c4e98957168e7
문찬웅 <mooncw@hankookilbo.com>
2024-08-06 18:02:09 +0900

    pm2 start script added

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -7,4 +7,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 npm install
-pm2 start dist
+# pm2 start dist
+pm2 start npm --name "hkqa-server" -- run "start:prod"
