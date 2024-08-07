2a828503b66e3e5b0936ca98b6167b05f89b5f5d
문찬웅 <mooncw@hankookilbo.com>
2024-08-07 09:50:48 +0900

    fix on script file

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+// import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/HttpExceptionFilter';
 
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,
-
-  );
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     // new ValidationPipe({ whitelist: true }),
   );
   app.useGlobalFilters(new HttpExceptionFilter())
 
   app.enableCors({
-    origin: true,
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   })
