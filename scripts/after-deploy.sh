0fc035f46e0fa94d574ba18a66ece1924eace691
root <cksdnd004@naver.com>
2024-08-22 18:31:40 +0900

    deploy with spec up ec2

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -6,7 +6,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-npm ci
+npm install
 
 pm2 stop hkqa-server
 
