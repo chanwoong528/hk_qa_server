2fe7d5309a584d1ca2d4fb213a9928dadaa20fbb
Chanwoong Moon <64773391+chanwoong528@users.noreply.github.com>
2024-08-22 15:58:13 +0900

    Update after-deploy.sh

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -6,8 +6,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-npm install
-
 pm2 stop hkqa-server
 
 pm2 start ./ecosystem.config.js 
